3c06365dabece01302b025170358069ca778f2c2
Thirukrishnan <thiru18krish@gmail.com>
2023-03-08 01:07:18 -0500

    Updated main

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask, jsonify
-import os
+from flask import *
+
+
 
 app = Flask(__name__)
+app.secret_key = 'thiran{sst1_th3_g1ft_th4t_k33ps_0n_g1v1ng}'
 
-
-@app.route('/')
+@app.route("/")
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('index.html')
 
+@app.route("/hello")
+def hello_ssti():
+    template=''
+    if request.args.get('name'):
+        name = request.args.get('name')
+        template = f'''<div>
+        <h1>Hello {name}</h1><center><img src="static/loser.jpg" alt="Loser Image" width="600" height="600"></center>
+        </div>
+        '''
+    return render_template_string(template)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+   app.run('127.1',7070,debug=True)
